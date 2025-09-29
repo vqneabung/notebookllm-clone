554360d00bb421dfca68cf3261162af52d99caeb
Vũ Quang Nguyên <nguyenvqse180208@fpt.edu.vn>
2025-09-29 08:46:09 +0700

    refactor: simplify similarity calculation in findRelevantContent function

--- lib/ai/embedding.ts
+++ lib/ai/embedding.ts
@@ -33,10 +33,14 @@ export const generateEmbedding = async (value: string): Promise<number[]> => {
 
 export const findRelevantContent = async (userQuery: string) => {
   const userQueryEmbedded = await generateEmbedding(userQuery);
-  const similarity = sql<number>`1 - (${cosineDistance(
+  // const similarity = sql<number>`1 - (${cosineDistance(
+  //   embeddings.embedding,
+  //   userQueryEmbedded,
+  // )})`;
+  const similarity = sql<number>`${cosineDistance(
     embeddings.embedding,
     userQueryEmbedded,
-  )})`;
+  )}`;
   const similarGuides = await db
     .select({ name: embeddings.content, similarity })
     .from(embeddings)
